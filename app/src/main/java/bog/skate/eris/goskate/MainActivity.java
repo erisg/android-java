4418668f80d185dbcaf9cc3d8bf2b72e03a3cf92
geral <erisbg.513@gmail.com>
2019-01-08 22:33:07 -0500

    lo y pos

--- app/src/main/java/bog/skate/eris/goskate/MainActivity.java
+++ app/src/main/java/bog/skate/eris/goskate/MainActivity.java
@@ -8,12 +8,18 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-    private ImageButton PostButton;
+
+    private ImageButton post_button;
+    private ImageButton select_post;
+
+
+    private static final int Gallery_Pick =1;
 
 
     @Override
@@ -24,10 +30,19 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(this);
 
-        PostButton = (ImageButton) findViewById(R.id.post_button);
+        post_button = (ImageButton) findViewById(R.id.post_button);
+        post_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
 
+            }
+        });
+        
 
     }
+
+
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
---
+++ app/src/main/java/bog/skate/eris/goskate/registroActivity.java
@@ -0,0 +1,13 @@
+package bog.skate.eris.goskate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class registroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
