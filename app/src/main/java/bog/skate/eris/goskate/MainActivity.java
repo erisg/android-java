31396b5428a898caf01bfd3e63232a36f7811def
geral <erisbg.513@gmail.com>
2019-01-09 20:45:34 -0500

    send

--- app/src/main/java/bog/skate/eris/goskate/MainActivity.java
+++ app/src/main/java/bog/skate/eris/goskate/MainActivity.java
@@ -6,7 +6,9 @@ import android.content.IntentSender;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -15,18 +17,26 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-   // botones
+
+    // botones
 
     private ImageButton post_button;
-    private ImageButton select_post;
+    private ImageButton salir;
 
 
-    // autenticacion
+    // autenticacion firebase
 
     private FirebaseAuth mAuth;
+    private DatabaseReference UserRef;
 
 
 
@@ -36,30 +46,47 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+        UserRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(this);
 
         post_button = (ImageButton) findViewById(R.id.post_button);
-        post_button.setOnClickListener(new View.OnClickListener() {
+        post_button.setOnClickListener(new View.OnClickListener()
+
+        {
             @Override
             public void onClick(View view)
             {
 
             }
         });
-        
 
     }
 
+    private void UserMenuSelector(MenuItem item)
+    {
+        Activity activity = null;
+        switch (item.getItemId())
+        {
 
+            case R.id.navigation_home:
+            break;
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            case R.id.navigation_parches:
+                break;
 
-        return false;
+            case R.id.navigation_skateshop:
+                break;
+
+            case R.id.navigation_salir:
+                mAuth.signOut();
+                SendUserToLoginActivity();
+                break;
+        }
+
     }
-
 
   @Override
    protected void onStart()
@@ -72,10 +99,17 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
           SendUserToLoginActivity();
       }
 
-
-
-
   }
+
+
+
+    private void SendUserToSetupActivity()
+    {
+        Intent  setupIntent = new Intent(MainActivity.this, MainActivity.class);
+        setupIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        finish();
+    }
+
 
     private void SendUserToLoginActivity()
     {
@@ -85,4 +119,8 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
 }
